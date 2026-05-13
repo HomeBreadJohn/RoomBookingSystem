52fb8fd4b99040aef99b68241513b7b2b7f2674a
Ron <@puyat_ronjacob@plpasig.edu.ph>
2026-05-13 19:15:34 +0800

    added Receiptsgenerator

--- src/utils/ReceiptGenerator.java
+++ src/utils/ReceiptGenerator.java
@@ -1,4 +1,22 @@
-package utils;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
 public class ReceiptGenerator {
+    public ReceiptGenerator() {
+    }
+
+    public static void printReceipt(String name, String roomType, int nights, int total) {
+        System.out.println("\n===== RECEIPT =====");
+        System.out.println("Guest Name: " + name);
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Nights: " + nights);
+        System.out.println("Total Payment: ₱" + total);
+        System.out.println("===================\n");
+    }
+
+    public static String generateReceiptData(String name, String roomType, int nights, int total) {
+        return name + "," + roomType + "," + nights + "," + total;
+    }
 }
