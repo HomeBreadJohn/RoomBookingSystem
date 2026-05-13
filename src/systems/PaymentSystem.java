93515f3e29774121a8459a27e6651da9ae2ec123
Ron <@puyat_ronjacob@plpasig.edu.ph>
2026-05-13 18:52:55 +0800

    added PaymentSystem

--- src/systems/PaymentSystem.java
+++ src/systems/PaymentSystem.java
@@ -1,4 +1,29 @@
-package systems;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
 public class PaymentSystem {
+    public PaymentSystem() {
+    }
+
+    public static int computeTotal(String roomType, int nights) {
+        int pricePerNight = 0;
+        switch (roomType.toUpperCase()) {
+            case "ROOM_A":
+                pricePerNight = 500;
+                break;
+            case "ROOM_B":
+                pricePerNight = 1000;
+                break;
+            case "ROOM_C":
+                pricePerNight = 1500;
+                break;
+            default:
+                System.out.println("Invalid Room Type.");
+                return 0;
+        }
+
+        return pricePerNight * nights;
+    }
 }
