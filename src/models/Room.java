a3f33cc7e067cf9992c9c6d93787aeb683c593f2
zacluther <salceda.zacluther@gmail.com>
2026-05-13 18:42:08 +0800

    Implemented booking management

--- src/models/Room.java
+++ src/models/Room.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Room {
+    String roomName;
+    double roomPrice;
+
+    public Room(String roomName, double roomPrice) {
+        this.roomName = roomName;
+        this.roomPrice = roomPrice;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public double getRoomPrice() {
+        return roomPrice;
+    }
 }
