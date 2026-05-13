adfebba8b5054d8ecf7dfd3ad52f51d9e3bc88e5
Ron <@puyat_ronjacob@plpasig.edu.ph>
2026-05-13 19:16:34 +0800

    added constants

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,4 +1,8 @@
-package utils;
+public class constants {
+    public static final int ROOM_A_PRICE = 500;
+    public static final int ROOM_B_PRICE = 1000;
+    public static final int ROOM_C_PRICE = 1500;
 
-public class Constants {
+    public constants() {
+    }
 }
